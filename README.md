871c06167c13a91b4633bdc66dd3f36fc0b6d7e3
frappe <frappe@Dev1>
2023-11-01 22:25:09 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Casper Etl
+
+App to Manage Data Migration Projects
+
+#### License
+
+MIT
---
+++ casper_etl/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Casper Etl",
+			"type": "module",
+			"label": _("Casper Etl")
+		}
+	]
