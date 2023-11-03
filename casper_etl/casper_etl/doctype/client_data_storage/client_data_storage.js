89779bc937ac1844cae8c66a045ead0868ef627b
jsbhullar <jsbhullar@gmail.com>
2023-11-03 22:58:37 +0530

    added projects , client data storage and worked  with SI,Vendor and Clients

---
+++ casper_etl/casper_etl/doctype/client_data_storage/client_data_storage.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, js bhullar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Client Data Storage', {
+	// refresh: function(frm) {
+
+	// }
+});
